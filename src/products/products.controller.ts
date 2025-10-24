90b11d7203ad04c1d6f7dfa41ae7d76b82f35e21
Wesly <agustchannel@gmail.com>
2025-10-24 13:01:28 +0700

    feat: initialize create product route

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { IsString, IsNumber, IsDate, IsNotEmpty } from 'class-validator';
+
+class ProductDTO {
+    @IsString({message: "Name must be a string"})
+    name: string;
+
+    @IsNotEmpty()
+    @IsNumber({allowNaN: false, allowInfinity: false})
+    price: number;
+
+    @IsNotEmpty()
+    @IsNumber({allowNaN: false, allowInfinity: false})
+    qty: number;
+}
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    @Post()
+    async create(@Body() productDto: ProductDTO): Promise<string>{
+        console.log(productDto)
+        return "creating product..."
+    }
+
+}
