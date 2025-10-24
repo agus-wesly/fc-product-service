2c4b4fc0f8a5ef26abb9022f5997d0e733021933
Wesly <agustchannel@gmail.com>
2025-10-24 13:01:08 +0700

    feat: setup validation library

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+      new ValidationPipe({
+          whitelist: true,
+          exceptionFactory: (errors) => {
+              const formattedErrors = {};
+
+              errors.forEach((error) => {
+                  if (error.constraints) {
+                    formattedErrors[error.property] = Object.values(error.constraints)[0];
+                  }
+              });
+
+              return new BadRequestException({
+                  statusCode: 400,
+                  message: 'Validation failed',
+                  errors: formattedErrors,
+              });
+          }
+
+      })
+  )
   await app.listen(process.env.PORT ?? 6969);
 }
 bootstrap();
