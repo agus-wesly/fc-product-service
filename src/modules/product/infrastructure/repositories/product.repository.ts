5d7ef9b014f068f585671c81bc526a90e540e9a6
Wesly <agustchannel@gmail.com>
2025-10-25 09:06:08 +0700

    fix: remove the manually created repository file

--- src/modules/product/infrastructure/repositories/product.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-export class ProductRepository {
-    async save() {
-        return "saving..."
-        // logic to save into db
-    }
-}
