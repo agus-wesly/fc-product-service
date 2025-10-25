6262754e37bad22fb897b8c636b243da20859055
Wesly <agustchannel@gmail.com>
2025-10-25 13:10:47 +0700

    fix: remove unecessary stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './modules/product/product.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
-import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './config/app-options.constants';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
         ProductModule,
         DatabaseModule,
         RedisModule,
