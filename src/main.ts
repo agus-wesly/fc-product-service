3e48fa25992180ac21d09991018e30309729225e
Wesly <agustchannel@gmail.com>
2025-10-25 15:52:53 +0700

    feat: add rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-      new ValidationPipe({
-          whitelist: true,
-          exceptionFactory: (errors) => {
-              const formattedErrors = {};
+    const app = await NestFactory.create(AppModule);
 
-              errors.forEach((error) => {
-                  if (error.constraints) {
-                    formattedErrors[error.property] = Object.values(error.constraints)[0];
-                  }
-              });
+    app.connectMicroservice(
+        {
+            transport: Transport.RMQ,
+            options: {
+                urls: ['amqp://localhost:5672'],
+                queue: 'app_queue',
+                queueOptions: {
+                    durable: true,
+                },
+            }
+        }
+    )
+    await app.startAllMicroservices();
 
-              return new BadRequestException({
-                  statusCode: 400,
-                  message: 'Validation failed',
-                  errors: formattedErrors,
-              });
-          }
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            exceptionFactory: (errors) => {
+                const formattedErrors = {};
 
-      })
-  )
-  await app.listen(process.env.PORT ?? 6969);
+                errors.forEach((error) => {
+                    if (error.constraints) {
+                        formattedErrors[error.property] = Object.values(error.constraints)[0];
+                    }
+                });
+
+                return new BadRequestException({
+                    statusCode: 400,
+                    message: 'Validation failed',
+                    errors: formattedErrors,
+                });
+            }
+
+        })
+    )
+
+    await app.listen(process.env.PORT ?? 6969);
 }
 bootstrap();
