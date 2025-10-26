559b0442bc5cc13b3741db33e0fb59c1e525ff75
Wesly <agustchannel@gmail.com>
2025-10-26 11:26:35 +0700

    feat: emit event after creating new product

--- src/modules/product/products.controller.ts
+++ src/modules/product/products.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Post, Body, Param, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseInterceptors, Inject } from '@nestjs/common';
 import { ProductsService } from "./application/services/products.service"
 import { CreateProductDTO } from './application/dtos/create-product.dto';
 import {  CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('products')
 export class ProductsController {
     constructor(
         private productServices: ProductsService,
+        @Inject('ORDER_SERVICE') private client: ClientProxy,
     ) { }
 
     @Post()
     async create(@Body() productDto: CreateProductDTO) {
-        return this.productServices.create(productDto)
+        const newProduct = await this.productServices.create(productDto)
+        this.client.send('product.created', newProduct)
+        return newProduct
     }
 
     @UseInterceptors(CacheInterceptor)
